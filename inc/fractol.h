d206a1cf808c6b13029bfaad677dc355b3b69241
Shalyca Sottoriva <ssottori@c3r4s1.42london.com>
2024-04-14 14:37:51 +0100

    ok

--- inc/fractol.h
+++ inc/fractol.h
@@ -6,13 +6,14 @@
 /*   By: ssottori <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 09:35:54 by ssottori          #+#    #+#             */
-/*   Updated: 2024/04/13 02:06:32 by ssottori         ###   ########.fr       */
+/*   Updated: 2024/04/14 14:29:50 by ssottori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 # define FRACTOL_H
 
+// =======LIBRARIES
 # include "mlx.h"
 # include "libft.h"
 # include <fcntl.h>
@@ -20,11 +21,41 @@
 # include <stdarg.h>
 # include <stdlib.h>
 # include <stdio.h>
+# include <math.h>
 
-# define IMG_W 32
-# define IMG_H 32
+// ========WINDOW SIZE
+# define WIDTH 1000
+# define HEIGHT 1000
 
-int	map_name(const char *file);
-void	sl_error(char *err);
+// ========COLORS
+# define BLACK			0x000000
+# define WHITE			0xFFFFFF
+# define GREEN			0x00FF00
+# define BLUE			0x0000FF
+# define RED      0xFF0000
+# define MAGENTA			0xFF00FF
+# define LIME			0xCCFF00
+# define ORANGE			0xFF6600
+# define PURPLE			0x9932CC
+# define CYAN			0x33CCCC
+# define PINK			0xFF66B2
+# define ELECTRIC		0x0066FF
+# define LIGHTNING		0x00CCFF
+# define LAVA			0xFF3300
+# define YELLOW			0xFFFF00
+# define PASTELYELLOW	0xFFFF99
+# define	PASTELPINK		0xFFB6C1
+# define LILAC      0xEFB1FF
+
+typedef struct s_fractol
+{
+
+  void  *mlx_init_bruv;
+  void  *mlx_window;
+  t_image img;
+
+} t_fractol;
+
+void  ft_pixel_put(t_fractol *fractol, int x, int y, int rgb);
 
 #endif
--- window.c
+++ window.c
@@ -6,35 +6,52 @@
 /*   By: ssottori <ssottori@student.42london.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 23:59:00 by ssottori          #+#    #+#             */
-/*   Updated: 2024/04/13 02:20:35 by ssottori         ###   ########.fr       */
+/*   Updated: 2024/04/14 14:37:31 by ssottori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
-
-#include <mlx.h>
-
 #include <mlx.h>
 #include <stdio.h>
 
-typedef struct	s_vars {
-	void	*mlx;
-	void	*win;
-}				t_vars;
-
-int	key_hook(int keycode, t_vars *vars)
+/*void  *ft_pixel_put(t_fractol *fractol, int x, int y, int rgb)
 {
-	printf("Hello from key_hook!\n");
-	return (0);
+  char *dst;
+
+  if (x >= 0 && < WIDTH && y >= 0 && < HEIGHT)
+  {
+    dst = fractol->img_line + 
+      (y * fractol->image.line_length + x * (fractol->img.bits_per_pixel / 8));
+    *(unsigned int *)dst = rgb;
+  }
+}*/
+
+void  ft_init_fractol(t_fractol *ractol, int ac, char **av)
+{
+  ft_args_check(fractol, ac, av);
+{
+
+void  ft_args_checks(t_fractol *fractol, int ac, char **av)
+{
+  if (ac < 2 || ac > 4)
+    ft_err_msg();
+
+void  ft_init_window(t_fractol *fractol)
+{
+  fractol->mlx_init_bruv = mlx_init();
+  if (!fractol)
+    exit(1);
+  fractol->mlx_window = mlx_new window(fractol->mlx_init_bruv, 
+      WIDTH, HEIGHT, "Fract-ol <3");
+  if (!fractol->mlx_window)
+    exit(1);
 }
 
 int	main(void)
 {
-	t_vars	vars;
+  t_fractol fractol;
+  ft_init_window(fractol);
 
-	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 640, 480, "Hello world!");
-	mlx_key_hook(vars.win, key_hook, &vars);
-	mlx_loop(vars.mlx);
+	mlx_string_put(mlx_init_bruv, mlx_win, WIDTH * 0.5, HEIGHT * 0.5, 0xeda7ff, "Hello Donut! <3");
+	mlx_loop(mlx_init_bruv);
 }
-

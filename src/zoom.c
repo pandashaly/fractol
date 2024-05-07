3d80420947f75e339620b7d5bbf48ca82af62c6c
Shalyca Sottoriva <ssottori@c3r4s1.42london.com>
2024-05-08 00:05:05 +0100

    not working

--- src/zoom.c
+++ src/zoom.c
@@ -23,21 +23,20 @@ int	ft_mouse_hook(int button, int x, int y, t_fractol *fract)
     {
 		fract->zoom += ZOOM;
         ft_bullseye(fract, mouse_x, mouse_y);
-        ft_init_fractol(fract);
     }
 	else if (button == M_ZOOM_OUT)
     {
 		fract->zoom -= ZOOM;
         ft_bullseye(fract, mouse_x, mouse_y);
-        ft_init_fractol(fract);
     }
+    ft_init_fractol(fract);
 	return (0);
 }
 
 void ft_bullseye(t_fractol *fract, double mouse_x, double mouse_y)
 {
-    fract->shift_r = (mouse_x * (MAX_REAL - MIN_REAL)) * fract->zoom;
-    fract->shift_i = (mouse_y * (MAX_IMAGINARY - MIN_IMAGINARY)) * fract->zoom;
+    fract->shift_r = (mouse_x - (MAX_REAL - MIN_REAL)) * fract->zoom;
+    fract->shift_i = (mouse_y - (MAX_IMAGINARY - MIN_IMAGINARY)) * fract->zoom;
     printf("\nmouse %f, %f\n", mouse_x, mouse_y);
     printf("center %d, %d\n", WIDTH / 2, HEIGHT / 2);
     printf("shift r_i %f, %f\n", fract->shift_r, fract->shift_i);
